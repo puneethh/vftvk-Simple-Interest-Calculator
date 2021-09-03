b3d0498c0b4e4de8febd139969dd42914ce818d9
Punit Srivastava <puneethh@yahoo.com>
2021-09-03 13:14:16 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function compute() {
 
 }
 
-//update the rate value
+//update the rate value in percent
 function updateRate() {
     document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
 }
